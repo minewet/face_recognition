aac13774f57f2cea5eb8e7396fa8d24d654e44ea
minewet <71055964+minewet@users.noreply.github.com>
2021-07-09 14:29:10 +0900

    Update dnndetection.py

--- dnn_detection/dnndetection.py
+++ dnn_detection/dnndetection.py
@@ -5,9 +5,9 @@ import cv2
 
 # load model
 face_detector = ""
-prototxt = "./face_detector/deploy.prototxt.txt" # prototxt 파일 : 모델의 레이어 구성 및 속성 정의
-weights = "./face_detector/res10_300x300_ssd_iter_140000.caffemodel" # caffemodel 파일 : 얼굴 인식을 위해 ResNet 기본 네트워크를 사용하는 SSD(Single Shot Detector) 프레임워크를 통해 사전 훈련된 모델 가중치 사용
-net = cv2.dnn.readNet(prototxt, weights) # cv2.dnn.readNet() : 네트워크를 메모리에 로드
+prototxt = "./face_detector/deploy.prototxt.txt" #모델의 레이어 구성 및 속성 정의
+weights = "./face_detector/res10_300x300_ssd_iter_140000.caffemodel" # caffemodel: 얼굴 인식을 위해 ResNet 기본 네트워크를 사용하는 SSD(Single Shot Detector) 프레임워크를 통해 사전 훈련된 모델 가중치 사용
+net = cv2.dnn.readNet(prototxt, weights) #네트워크를 메모리에 로드
 
 
 image = cv2.imread("./input/3.jpg")
